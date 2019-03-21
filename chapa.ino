268c6713747443a1051fd0c707e3dadfa3013105
Alejandro Roiz Walss <alejandro.roiz.walss@gmail.com>
2019-03-21 15:27:36 -0600

    init git repo

---
+++ chapa.ino
@@ -0,0 +1,16 @@
+// CHIP RFID
+#include <MFRC522.h>
+#include <require_cpp11.h>
+#include <MFRC522Extended.h>
+#include <deprecated.h>
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
